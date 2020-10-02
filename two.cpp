882ef074e0408993920b9d82204a5a7ed335fe5a
Tushar Saxena <stushar345@gmail.com>
2020-10-02 18:50:16 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1="geksfor";
+    set<int> s;
+    int flag=0;
+    for(int i=0;i<s1.length();i++)
+    {
+        auto c1= s1.find(s1[i]);
+        if(s1.find(s1[i],c1+1)<s1.length())
+        {
+            cout<<c1;
+            flag=1;
+            break;
+        }
+        
+    }
+    if(flag==0)
+    {
+        cout<<"-1";
+    }
+}
